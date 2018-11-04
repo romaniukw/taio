75886084f15da69ff17e340c3e46a31be1a31064
Weronika Romaniuk <veronka106@interia.pl>
2018-11-04 14:43:39 +0100

    Read graphs from file.

--- Isomorphism/Program.cs
+++ Isomorphism/Program.cs
@@ -11,10 +11,10 @@ namespace Isomorphism
         static void Main(string[] args)
         {
             int[,] tab = new int[,] { { 0, 1 }, { 1, 0 } };
-            int[,] fullGraph = new int[,] { { 0, 1, 1, 1, 1 }, { 1, 0, 1, 1, 1 }, { 1, 1, 0, 1, 1 }, { 1, 1, 1, 0, 1 }, { 1, 1, 1, 1, 0 } };
 
-            Graph G = new Graph(fullGraph);
-            Graph H = new Graph(fullGraph);
+            Graph G, H;
+            ReadGraphsFromFile.GetGraphs("../../Data/data1.txt", out G, out H);
+
             SearchSubGraph subGraph;
             if (G.Vertices.Length <= H.Vertices.Length)
                 subGraph = new SearchSubGraph(G, H);
@@ -22,25 +22,6 @@ namespace Isomorphism
                 subGraph = new SearchSubGraph(H, G);
 
             Console.WriteLine(subGraph.SubGraph.Vertices.Length);
-
-            //int[,] isofirst = new int[,] { { 0, 0, 1, 0, 0, 0, 0 },
-            //                               { 0, 0, 1, 0, 0, 0, 0 },
-            //                               { 1, 1, 0, 1, 0, 0, 0 },
-            //                               { 0, 0, 1, 0, 1, 0, 1 },
-            //                               { 0, 0, 0, 1, 0, 1, 0 },
-            //                               { 0, 0, 0, 0, 1, 0, 0 },
-            //                               { 0, 0, 0, 1, 0, 0, 0 } };
-            //int[,] isosecond = new int[,] { { 0, 1, 0, 0, 0, 0, 0 }, 
-            //                                 { 1, 0, 0, 0, 0, 1, 0 }, 
-            //                                 { 0, 0, 0, 0, 0, 1, 0 }, 
-            //                                 { 0, 0, 0, 0, 0, 0, 1 }, 
-            //                                 { 0, 0, 0, 0, 0, 0, 1 }, 
-            //                                 { 0, 1, 1, 0, 0, 0, 1 }, 
-            //                                 { 0, 0, 0, 1, 1, 1, 0 } };
-
-            //Graph G = new Graph(fullGraph);
-            //Graph first = new Graph(isofirst);
-            //Graph second = new Graph(isosecond);
             //bool t = FullIsomorphismChecker.AreTheyIsomorphic(first, second);
         }
     }
---
+++ Isomorphism/ReadGraphsFromFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Isomorphism
+{
+    public static class ReadGraphsFromFile
+    {
+        public static void GetGraphs(string path, out Graph G, out Graph H)
+        {
+            StreamReader sr = new StreamReader(path);
+            var s = sr.ReadToEnd();
+            var t = s.Split(new[] { "\r\n\r\n" }, StringSplitOptions.None);
+
+            ReadGraph(t[0], out G);
+            ReadGraph(t[1], out H);
+        }
+
+        private static void ReadGraph(string str, out Graph G)
+        {
+            var gtab = str.Split(new[] { "\r\n" }, StringSplitOptions.None)
+                          .Select(x => x.Split(','))
+                          .Select(x => Array.ConvertAll(x, int.Parse))
+                          .ToArray();
+
+            var twoD = new int[gtab.Length, gtab[0].Length];
+            for (int i = 0; i != gtab.Length; i++)
+                for (int j = 0; j != gtab[0].Length; j++)
+                    twoD[i, j] = gtab[i][j];
+
+            G = new Graph(twoD);
+        }
+
+    }
+}
