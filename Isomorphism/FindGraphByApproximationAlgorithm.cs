944cd3fae7d6e6fd03d4c7f8199f0148b6160677
Weronika Romaniuk <veronka106@interia.pl>
2018-11-18 21:46:18 +0100

    Add working aproximation algorithm.

--- Isomorphism/FindGraphByApproximationAlgorithm.cs
+++ Isomorphism/FindGraphByApproximationAlgorithm.cs
@@ -10,52 +10,59 @@ namespace Isomorphism
     {
         public static List<int[]> Search(Graph G, Graph H)
         {
-            List<int[]> result;
-
+            List<int[]> result = new List<int[]>();
             if (CheckSimpleCases(G, H, out result)) return result;
 
-            var GSubGraphs = GenerateSubGraphs(G);
+            var GSubGraphs = GenerateSubGraphs(G);      
             var HSubGraphs = GenerateSubGraphs(H);
-            
-            foreach(var gsub in GSubGraphs)
+
+            foreach (var gsub in GSubGraphs)
             {
                 foreach(var hsub in HSubGraphs)
                 {
                     var res = GenerateMaxIsomorphism(gsub, hsub);
+                    if (result == null || res[0].Length > result[0].Length) result = res;
+                    if(result[0].Length==G.Vertices.Length || result[0].Length==H.Vertices.Length)
+                    {
+                        return result;
+                    }
                 }
             }
-            return null;
+            return result;
         }
 
         private static bool CheckSimpleCases(Graph G, Graph H, out List<int[]> result)
         {
-            result = null;
-            if(G.Vertices.Length==0 || H.Vertices.Length == 0)
+            result = new List<int[]>();
+            if (G.Vertices.Length==0 || H.Vertices.Length == 0)
             {
+                result = new List<int[]>() { new int[0], new int[0] };
                 return true; //graf G lub H jest grafem pustym
             }
 
             if(G.Edges.Count==0 || H.Edges.Count==0)
             {
-                result.Add(new int[] { G.Vertices[0].Index, H.Vertices[0].Index });
+                result.Add(new int[] { G.Vertices[0].Index });
+                result.Add(new int[] { H.Vertices[0].Index });
                 return true;
             }
 
             if(G.Edges.Count == 1 || H.Edges.Count==1)
             {
-                result.Add(new int[] { G.Edges[0].From, H.Edges[0].From });
-                result.Add(new int[] { G.Edges[0].To, H.Edges[0].To });
+                result.Add(new int[] { G.Edges[0].From, G.Edges[0].To });
+                result.Add(new int[] { H.Edges[0].From, H.Edges[0].To });
                 return true;
             }
+            result.Add(new int[] { G.Vertices[0].Index });
+            result.Add(new int[] { H.Vertices[0].Index });
             return false;
         }
 
         private static List<SubGraph> GenerateSubGraphs(Graph G)
         {
-             List<SubGraph> subGraphs = new List<SubGraph>();
+            List<SubGraph> subGraphs = new List<SubGraph>();
             foreach(var v1 in G.Vertices)
             {
-
                 foreach(var v2 in v1.Neighbors)
                 {
                     foreach(var v3 in v2.Neighbors)
@@ -89,18 +96,23 @@ namespace Isomorphism
         }
         private static List<int[]> GenerateMaxIsomorphism(SubGraph subG, SubGraph subH)
         {
-            if (subG.Edges.Count != subH.Edges.Count) return null;  //na pewno null? czy mapowanie lini?
+            List<int[]> result = new List<int[]>() { new int[0], new int[0] };
+            if (subG.Edges.Count != subH.Edges.Count)
+            {
+                return new List<int[]>() { new int[] { subG.Edges[0].From, subG.Edges[0].To }, new int[] { subH.Edges[0].From, subH.Edges[0].To } };
+            }
+
             foreach(var mapG in subG.Maps)
             {
                 foreach(var mapH in subH.Maps)
                 {
-
-                    MappingGeneratorAndChecker mac = new MappingGeneratorAndChecker(subG, subH, mapG, mapH, G, H);
-                    var result = mac.GetMaxMapping();
+                    MappingGeneratorAndChecker mac = new MappingGeneratorAndChecker(subG, subH, mapG, mapH);
+                    var res = mac.GetMaxMapping();
+                    if (res[0].Count() > result[0].Count()) result = res;
                 }
             }
 
-            return null;
+            return result;
         }        
     }
 }
--- Isomorphism/MappingGeneratorAndChecker.cs
+++ Isomorphism/MappingGeneratorAndChecker.cs
@@ -8,8 +8,8 @@ namespace Isomorphism
 {
     class MappingGeneratorAndChecker
     {
-        private Graph gBig;
-        private Graph hBig;
+        private Graph gBase;
+        private Graph hBase;
 
         private List<Vertex> gVertices;
         private List<Vertex> gNeighbours;
@@ -20,33 +20,75 @@ namespace Isomorphism
         private List<Edge> hEdges;
 
         private List<int[]> mapping;
+        private int[] tabmapping;
 
         public MappingGeneratorAndChecker(SubGraph G, SubGraph H, int[] Gmap, int[] Hmap)
         {
-            gBig = G.BaseGraph;
-            gVertices = H.Vertexes;
-            gNeighbours = H.Neighbours;
-            gEdges = H.Edges;
-            hBig = H.BaseGraph;
-            hVertices = H.Vertexes;
-            hNeighbours = H.Neighbours;
-            hEdges = H.Edges;
+            gBase = G.BaseGraph;
+            gVertices = new List<Vertex>(G.Vertexes);
+            gNeighbours = new List<Vertex>(G.Neighbours);
+            gEdges = new List<Edge>(G.Edges);
+            hBase = H.BaseGraph;
+            hVertices = new List<Vertex>(H.Vertexes);
+            hNeighbours = new List<Vertex>(H.Neighbours);
+            hEdges = new List<Edge>(H.Edges);
 
-            var mapping = new List<int[]> { new int[] { Gmap[0], Hmap[0] },
-                                            new int[] { Gmap[1], Hmap[1] },
-                                            new int[] { Gmap[1], Hmap[1] } };
+            mapping = new List<int[]> {Gmap, Hmap};
+            tabmapping = new int[gBase.Vertices.Count()];
+            for(int i=0; i<tabmapping.Length; i++)
+            {
+                tabmapping[i] = -1;
+            }
+            for (int i=0; i<Gmap.Length; i++)
+            {
+                tabmapping[Gmap[i]] = Hmap[i];
+            }
         }
 
         public List<int[]> GetMaxMapping()
         {
-            var gv = gNeighbours.Aggregate((i1, i2) => i1.Degree > i2.Degree ? i1 : i2);
-            gVertices.Add(gv); //dodaj do wierzchołków
-            //dodaj krawędzie
-            //sprawdż mapowanie jeżeli ok to jak nie to return mapping
-            gNeighbours.Remove(gv); //usuń z sąsiadów
-            //dodaj sąsiadów
+            if(mapping[0].Length==gBase.Vertices.Length || mapping[1].Length == hBase.Vertices.Length)
+            {
+                return mapping;
+            }
+            var gv = gNeighbours.Aggregate((i1, i2) => i1.Degree > i2.Degree ? i1 : i2); 
+            var newgedges = gBase.Edges.Where(x => (x.From == gv.Index && x.To > gv.Index && mapping[0].Contains(x.To))
+                            || (x.To == gv.Index && x.From < gv.Index && mapping[0].Contains(x.From))).ToList();
+            
+            var hv = hNeighbours.Aggregate((i1, i2) => i1.Degree > i2.Degree ? i1 : i2);
+            var newhedges = hBase.Edges.Where(x => (x.From == hv.Index && x.To > hv.Index && mapping[1].Contains(x.To))
+                            || (x.To == hv.Index && x.From < hv.Index && mapping[1].Contains(x.From))).ToList();
 
-            return null;
+            tabmapping[gv.Index] = hv.Index;
+            if(newgedges.Count!=newhedges.Count) return mapping;
+            foreach(var e in newgedges)
+            {
+                if(!newhedges.Select(x=>x).Where(x=> (x.From==tabmapping[e.From] && x.To== tabmapping[e.To]) || (x.To== tabmapping[e.From] && x.From == tabmapping[e.To])).Any())
+                {
+                    return mapping;
+                }
+            }
+
+            var temp = mapping[0].ToList();
+            temp.Add(gv.Index);
+            mapping[0] = temp.ToArray();
+
+            temp = mapping[1].ToList();
+            temp.Add(hv.Index);
+            mapping[1] = temp.ToArray();
+
+            gVertices.Add(gv);
+            gEdges.AddRange(newgedges);
+            gNeighbours.Remove(gv); 
+
+            hVertices.Add(hv); 
+            hEdges.AddRange(newhedges);
+            hNeighbours.Remove(hv);
+
+            gNeighbours.AddRange(gv.Neighbors.Select(x=>x).Where(x => !gNeighbours.Contains(x) && !gVertices.Contains(x)).ToList());
+            hNeighbours.AddRange(hv.Neighbors.Select(x => x).Where(x => !hNeighbours.Contains(x) && !hVertices.Contains(x)).ToList());
+
+            return GetMaxMapping();
         }
     }
 }
